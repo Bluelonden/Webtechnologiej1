eecdcb0aa79d2e05ca2c3c87ad5a28149d624d18
Leon Molema <leon.r.molema@gmail.com>
2026-03-31 11:58:46 +0200

    diverse veranderingen

--- forms.py
+++ forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, EqualTo
 
+#Dit is voor de registratie van een gebruiker
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
@@ -9,6 +10,7 @@ class RegistrationForm(FlaskForm):
     validators=[DataRequired(), EqualTo('password')]
     submit = SubmitField('Register')
 
+# En dit is voor de login van de gebruiker
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])    
     password = PasswordField("Wachtwoord", render_kw={"Placeholder":"Wachtwoord"})
